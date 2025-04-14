2bca9216015311dddedd8a4572616a76183403dc
syn <tlsdpsk1992@naver.c0m>
2025-04-15 08:12:38 +0900

    [feat] 서비스 로직 작성, 매퍼 작성

---
+++ src/main/java/com/fouribnb/lodge/presentation/mapper/LodgeMapper.java
@@ -0,0 +1,37 @@
+package com.fouribnb.lodge.presentation.mapper;
+
+import com.fouribnb.lodge.domain.entity.Lodge;
+import com.fouribnb.lodge.domain.entity.LodgeStatus;
+import com.fouribnb.lodge.presentation.dto.request.CreateLodgeRequestDto;
+import com.fouribnb.lodge.presentation.dto.response.CreateLodgeResponseDto;
+
+public class LodgeMapper {
+
+    public static Lodge CreateLodgeRequestDtoToEntity(CreateLodgeRequestDto dto) {
+        return Lodge.builder()
+                .lodgeName(dto.getLodgeName())
+                .hostId(dto.getHostId())
+                .roomType(dto.getRoomType())
+                .address(dto.getAddress())
+                .capacity(dto.getCapacity())
+                .discription(dto.getDescription())
+                .pricePerNight(dto.getPricePerNignt())
+                .amenities(dto.getAmenities())
+                .lodgeStatus(LodgeStatus.IN_SERVICE)
+                .build();
+    }
+
+    public static CreateLodgeResponseDto EntityToCreateLodgeRequestDto(Lodge lodge) {
+        return CreateLodgeResponseDto.builder()
+                .lodgeId(lodge.getId())
+                .hostId(lodge.getHostId())
+                .roomType(lodge.getRoomType())
+                .address(lodge.getAddress())
+                .capacity(lodge.getCapacity())
+                .description(lodge.getDiscription())
+                .pricePerNignt(lodge.getPricePerNight())
+                .amenities(lodge.getAmenities())
+                .lodgeStatus(lodge.getLodgeStatus())
+                .build();
+    }
+}
