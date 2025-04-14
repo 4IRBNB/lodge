fb5f60a5caac5df0cee240186dbd22383c92c1e2
syn <tlsdpsk1992@naver.c0m>
2025-04-15 08:13:02 +0900

    [feat] dto 작성

---
+++ src/main/java/com/fouribnb/lodge/presentation/dto/request/CreateLodgeRequestDto.java
@@ -0,0 +1,21 @@
+package com.fouribnb.lodge.presentation.dto.request;
+
+import com.fouribnb.lodge.domain.entity.Amenities;
+import com.fouribnb.lodge.domain.entity.RoomType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Builder
+public class CreateLodgeRequestDto {
+
+    private Long hostId;
+    private String lodgeName;
+    private RoomType roomType;
+    private int capacity;
+    private String address;
+    private Long pricePerNignt;
+    private String description;
+    private Amenities amenities;
+}
