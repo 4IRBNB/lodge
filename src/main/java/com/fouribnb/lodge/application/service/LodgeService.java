5265adb548d680d2923b1548b38fd77fbbab1ec4
syn <tlsdpsk1992@naver.c0m>
2025-04-15 13:04:37 +0900

    [feat] service 작성

--- src/main/java/com/fouribnb/lodge/application/service/LodgeService.java
+++ src/main/java/com/fouribnb/lodge/application/service/LodgeService.java
@@ -1,8 +1,10 @@
 package com.fouribnb.lodge.application.service;
 
 import com.fouribnb.lodge.presentation.dto.request.CreateLodgeRequestDto;
+import com.fouribnb.lodge.presentation.dto.request.UpdateLodgeRequestDto;
 import com.fouribnb.lodge.presentation.dto.response.CreateLodgeResponseDto;
 import com.fouribnb.lodge.presentation.dto.response.GetLodgeResponseDto;
+import com.fouribnb.lodge.presentation.dto.response.UpdateLodgeResponseDto;
 import java.util.UUID;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 
@@ -12,4 +14,7 @@ public interface LodgeService {
     CreateLodgeResponseDto createLodge(CreateLodgeRequestDto request);
 
     GetLodgeResponseDto getLodge(UUID id) throws NotFoundException;
+
+    UpdateLodgeResponseDto updateLodge(UUID id, UpdateLodgeRequestDto request)
+            throws NotFoundException;
 }
