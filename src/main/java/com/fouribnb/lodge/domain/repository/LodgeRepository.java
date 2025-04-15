8cde0b2026726958940d2051f07326c4c6e30a9c
syn <tlsdpsk1992@naver.c0m>
2025-04-15 09:26:31 +0900

    [feat] mapper, repository 작성

--- src/main/java/com/fouribnb/lodge/domain/repository/LodgeRepository.java
+++ src/main/java/com/fouribnb/lodge/domain/repository/LodgeRepository.java
@@ -1,7 +1,11 @@
 package com.fouribnb.lodge.domain.repository;
 
 import com.fouribnb.lodge.domain.entity.Lodge;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface LodgeRepository {
     Lodge save(Lodge lodge);
+
+    Optional<Lodge> findById(UUID id);
 }
--- src/main/java/com/fouribnb/lodge/presentation/mapper/LodgeMapper.java
+++ src/main/java/com/fouribnb/lodge/presentation/mapper/LodgeMapper.java
@@ -4,6 +4,7 @@ import com.fouribnb.lodge.domain.entity.Lodge;
 import com.fouribnb.lodge.domain.entity.LodgeStatus;
 import com.fouribnb.lodge.presentation.dto.request.CreateLodgeRequestDto;
 import com.fouribnb.lodge.presentation.dto.response.CreateLodgeResponseDto;
+import com.fouribnb.lodge.presentation.dto.response.GetLodgeResponseDto;
 
 public class LodgeMapper {
 
@@ -34,4 +35,19 @@ public class LodgeMapper {
                 .lodgeStatus(lodge.getLodgeStatus())
                 .build();
     }
+
+    public static GetLodgeResponseDto EntityToGetLodgeRequestDto(Lodge lodge) {
+        return GetLodgeResponseDto.builder()
+                .lodgeId(lodge.getId())
+                .hostId(lodge.getHostId())
+                .roomType(lodge.getRoomType())
+                .address(lodge.getAddress())
+                .capacity(lodge.getCapacity())
+                .description(lodge.getDescription())
+                .pricePerNight(lodge.getPricePerNight())
+                .amenities(lodge.getAmenities())
+                .lodgeStatus(lodge.getLodgeStatus())
+                .build();
+    }
+
 }
