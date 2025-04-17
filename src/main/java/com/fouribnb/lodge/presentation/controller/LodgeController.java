890fb46edcbc536c55d442cab5aaa10b68960d61
syn <tlsdpsk1992@naver.c0m>
2025-04-17 12:42:46 +0900

    [feat] 객실목록조회 컨트롤러 작성

--- src/main/java/com/fouribnb/lodge/presentation/controller/LodgeController.java
+++ src/main/java/com/fouribnb/lodge/presentation/controller/LodgeController.java
@@ -7,15 +7,20 @@ import com.fouribnb.lodge.presentation.dto.response.CreateLodgeResponseDto;
 import com.fouribnb.lodge.presentation.dto.response.GetLodgeResponseDto;
 import com.fouribnb.lodge.presentation.dto.response.UpdateLodgeResponseDto;
 import com.fourirbnb.common.response.BaseResponse;
+import com.fourirbnb.common.response.Pagination;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,10 +55,24 @@ public class LodgeController {
         UpdateLodgeResponseDto responseDto = lodgeService.updateLodge(lodgeId, requestDto);
         return BaseResponse.SUCCESS(responseDto, "객실 수정 완료", HttpStatus.OK.value());
     }
-    ///api/lodges/{lodgeId}
 
     //객실목록조회
-    ///api/lodges
+    @GetMapping
+    public BaseResponse<List<GetLodgeResponseDto>> getLodges(Pageable pageable) {
+        Page<GetLodgeResponseDto> page = lodgeService.getLodges(pageable);
+
+        Pagination pagination = new Pagination(
+                page.getNumber(),
+                (long) page.getSize(),
+                page.getTotalPages(),
+                (int) page.getTotalElements()
+        );
+        return BaseResponse.SUCCESS(
+                page.getContent(),
+                "객실 목록 조회 완료",
+                pagination
+        );
+    }
 
     //host객실목록조회
     ///api/lodges/{userId}
