5329bdcd90766638ccf42bf357112960110673b3
syn <tlsdpsk1992@naver.c0m>
2025-04-17 12:43:10 +0900

    [feat] 객실목록조회 Reqpsitory 작성

--- src/main/java/com/fouribnb/lodge/domain/repository/LodgeRepository.java
+++ src/main/java/com/fouribnb/lodge/domain/repository/LodgeRepository.java
@@ -1,11 +1,16 @@
 package com.fouribnb.lodge.domain.repository;
 
 import com.fouribnb.lodge.domain.entity.Lodge;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface LodgeRepository {
     Lodge save(Lodge lodge);
 
     Optional<Lodge> findById(UUID id);
+
+    Page<Lodge> findAll(Pageable pageable);
 }
